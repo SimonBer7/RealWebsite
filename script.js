2e846d2aabf706370bd4f3c194a5fd380e2b13ce
Šimon Bernard <113086082+SimonBer7@users.noreply.github.com>
2023-05-08 17:49:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,28 +13,230 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
-class Hrac {
-    constructor(jmeno, prijmeni, cislo, vek, datum_narozeni, pozice, vyska, vaha, narodnost, img) {
-        this.jmeno = jmeno;
-        
+class Produkt {
+    constructor(nazev, cena, img) {
+        this.nazev = nazev;
+        this.cena = cena;
+        this.img = img;
+    }
+
+
+    getCard() {
+        return ` 
+        <div class="col-md-3">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="${this.img}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">${this.nazev}</h5>
+                        <p class="card-text">Cena: ${this.cena}</p>
+                        <a href="#" class="btn btn-primary">KOUPIT</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+}
+
+class Obchod {
+    constructor() {
+        this.produkty = [];
+        this.getFromWeb();
+    }
+
+    addProdukt(produkt) {
+        this.produkty.push(produkt);
+    }
+
+    printProducts() {
+        let html = "";
+
+        this.produkty.forEach(produkt => {
+            html += produkt.getCard();
+        });
+
+        document.getElementsByClassName(".row").innerHTML = html;
+    }
+
+
+    getFromWeb() {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/SimonBer7/RealWebsite/main/produkty.json",
+            dataType: "json",
+            success: function (data) {
+                data["produkty"].forEach(produkt => {
+
+                    this.addProdukt(new Produkt(
+                        produkt.nazev, 
+                        produkt.cena,
+                        produkt.img
+                    ));
+                    
+                });
+                printProducts();
+            },
+            error: function () { // error callback 
+                alert('Error with connection to website');
+            }
+        });
     }
 }
 
 
-
-
-
-$.ajax({
-    url: "https://raw.githubusercontent.com/SimonBer7/RealWebsite/main/hraci.json",
-    dataType: "json",
-    success: function (data) {
-        console.log(data);
-
-    },
-    error: function () { // error callback 
-        alert('Error with connection to website');
+class Hrac {
+    constructor(jmeno, prijmeni, cislo, vek, datum_narozeni, pozice, vyska, vaha, narodnost, img) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.cislo = cislo;
+        this.vek = vek;
+        this.datum_narozeni = datum_narozeni;
+        this.pozice = pozice;
+        this.vyska = vyska;
+        this.vaha = vaha;
+        this.narodnost = narodnost;
+        this.img = img;
     }
-});
+
+
+    getCard() {
+        return ` 
+        <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="${this.img}" alt="Avatar" style="width:300px;height:300px;">
+                    </div>
+                    <div class="flip-card-back">
+                        <h1 id="jmeno">${this.jmeno} ${this.prijmeni}</h1>
+                        <p id="datum_narozeni">${this.datum_narozeni}, (<span id="vek">${this.vek}</span> let)</p>
+                        <p id="pozice">${this.pozice}</p>
+                        <p>Èíslo dresu: <span id="cislo_dresu">${this.cislo}</span></p>
+                        <p>Výška: <span id="vyska">${this.vyska}</span> cm</p>
+                        <p>Váha: <span id="vaha">${this.vaha}</span> kg</p>
+                        <p>Národnost: <span id="narodnost">${this.narodnost}</span></p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+
+}
+
+
+class Evidence {
+    constructor() {
+        this.hraci = [];
+        this.loadData();
+    }
+
+    addHrac(hrac) {
+        this.hraci.push(hrac);
+    }
+
+    loadData() {
+        if (localStorage.getItem("hraci") == null) {
+            this.getFromWeb();
+            console.log("Data byly nacteny z webu");
+
+        } else if (localStorage.getItem("hraci") != null) {
+            this.getFromLocalStorage();
+            console.log("Data byly nacteny z local storage");
+
+        } else {
+            this.vymazLocalStorage();
+            console.error("Data byly vymazany");
+        }
+    }
+
+    ulozToLocalStorage() {
+        localStorage.setItem("hraci", JSON.stringify(this.hraci));
+    }
+
+    vymazLocalStorage() {
+        localStorage.removeItem("hraci");
+    }
+
+    getFromWeb() {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/SimonBer7/RealWebsite/main/hraci.json",
+            dataType: "json",
+            success: function (data) {
+                data["hraci"].forEach(hrac => {
+                    
+                    this.addHrac(new Hrac(
+                        hrac.jmeno,
+                        hrac.prijmeni,
+                        hrac.cislo,
+                        hrac.vek,
+                        hrac.datum_narozeni,
+                        hrac.pozice,
+                        hrac.vyska,
+                        hrac.vaha,
+                        hrac.narodnost,
+                        hrac.img
+                    ));
+                    
+                });
+                this.ulozToLocalStorage();
+                this.printPlayers();
+            },
+            error: function () { // error callback 
+                alert('Error with connection to website');
+            }
+        });
+
+    }
+
+    getFromLocalStorage() {
+        let hraci = JSON.parse(localStorage.getItem("hraci"));
+
+        hraci.forEach(hrac => {
+            this.addHrac(new Hrac(
+                hrac.jmeno,
+                hrac.prijmeni,
+                hrac.cislo,
+                hrac.vek,
+                hrac.datum_narozeni,
+                hrac.pozice,
+                hrac.vyska,
+                hrac.vaha,
+                hrac.narodnost,
+                hrac.img
+            ));
+        });
+        this.printPlayers();
+    }
+
+    printPlayers() {
+        let html = "";
+
+        this.hraci.forEach(hrac => {
+            html += hrac.getCard();
+        });
+
+        document.getElementsByClassName(".panel").innerHTML = html;
+
+    }
+
+
+
+
+}
+
+
+$(document).ready(function () {
+    var evidence = new Evidence();
+    var obchod = new Obchod();
+})
+
+
+
+
+
+
+
+
+
 
 
 
